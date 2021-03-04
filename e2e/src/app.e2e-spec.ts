8e3bc65aad11c4a7a8770822b2add4568e161fd5
bharatkalyan <s.bharatkalyan@gmail.com>
2021-03-04 12:14:12 +0530

    second commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('statisticsOfCoronaVirus app is running!');
+    expect(page.getTitleText()).toEqual('statisticsOfVirus app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/statisticsOfCoronaVirus'),
+      dir: require('path').join(__dirname, './coverage/statisticsOfVirus'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
